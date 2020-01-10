c312b20fe65e94eb766317a27f63b1cf9d53493c
Weston Ruter <westonruter@google.com>
2020-01-10 11:42:36 -0800

    Improve performance of popular theme check by only having one installed at a time

--- check-popular-themes.sh
+++ check-popular-themes.sh
@@ -2,16 +2,42 @@
 
 set -e
 
-i=1
+i=0
 total=100
 if [ ! -z $1 ]; then
   total="$1"
 fi
 
+mkdir -p public/wp-content/wporg-themes
+
 for theme in $(lando wp --skip-plugins --skip-themes query-popular-themes $total); do
   theme=$(tr -d '\r\n' <<< $theme)
-  echo "## $theme ($i of $total)"
-  bash check-theme.sh "$theme" || echo "Failed to check theme"
   i=$((i+1))
+
+  if [ -e "results/$theme" ]; then
+    echo "Results already obtained for theme: $theme"
+    continue
+  fi
+
+  echo "## $theme ($i of $total)"
+
+  lando wp --skip-plugins --skip-themes theme install "$theme" || continue
+
+  # Ensure the parent theme is installed (sometimes auto-installed other times not?)
+  parent_theme=$(lando wp get-parent-theme "$theme")
+  if [ ! -z "$parent_theme" ]; then
+    lando wp --skip-plugins --skip-themes theme install "$parent_theme" || continue
+  fi
+
+  bash check-theme.sh "$theme" || echo "Failed to check theme"
+
+  for theme_dir in public/wp-content/themes/*; do
+    theme_basename=$(basename "$theme_dir")
+    if [ ! -e "public/wp-content/wporg-themes/$theme_basename" ]; then
+      mv "$theme_dir" "public/wp-content/wporg-themes/$theme_basename"
+    else
+      rm -R "$theme_dir"
+    fi
+  done
   echo
 done
