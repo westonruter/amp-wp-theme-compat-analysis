3de5aa0e18faeb9e8ebfdc0bff39b9932e717eaf
Weston Ruter <westonruter@google.com>
2020-01-22 09:42:05 -0800

    Allow starting at offset

--- check-wporg-themes.sh
+++ check-wporg-themes.sh
@@ -2,17 +2,25 @@
 
 set -e
 
-i=0
 total=100
 if [ ! -z $1 ]; then
   total="$1"
 fi
 
+start=0
+if [ ! -z $2 ]; then
+  start="$2"
+fi
+
 mkdir -p public/wp-content/theme-directories/wporg-themes
 
+i=0
 for theme in $(lando wp --skip-plugins --skip-themes query-popular-themes $total); do
   theme=$(tr -d '\r\n' <<< $theme)
   i=$((i+1))
+  if [[ "$i" -lt "$start" ]]; then
+		continue
+  fi
 
   if [ -e "results/theme-directories/wporg-themes/$theme" ]; then
     echo "Results already obtained for theme: $theme"
